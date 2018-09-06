3a1c629209ff8d061cc6c1d07bf4d868cedc587a
masafumi terazono <hijirinhijirin@gmail.com>
2018-09-06 16:49:52 +0900

    test2

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,10 +1,10 @@
 class HelloWorld {
 	public static void main(String[] args){
-		System.out.println("My name is arisa (...'v'...)");
-
-		System.out.println("qwertyuiopasdfghjklzxcvbnm");
-
-		System.out.println("My name is Masafumi Terazono.");
+		for(int i=0; i<5; i++){
+			System.out.println("My name is arisa (...'v'...)");
+			System.out.println("qwertyuiopasdfghjklzxcvbnm");
+			System.out.println("My name is Masafumi Terazono.");
+		}
 
 	}
 }
